49308877b6e166a21df20f5bbcf1d9487afc753c
bawmee <ppr8769@likelion.org>
2019-02-25 02:33:00 +0900

    third

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'f6%ep9hye5gepb2lf$9$c^6zk59l!d
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
